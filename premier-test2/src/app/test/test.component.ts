f8d4dd4ee934091613ec0ae4e85efe3f9abb15cd
nouhaila <nouhaila.quahar.officiel@gmail.com>
2023-08-16 16:02:53 +0200

    premier commit

--- premier-test2/src/app/test/test.component.ts
+++ premier-test2/src/app/test/test.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
 })
-export class TestComponent {
+export class MainComponent {
 
+  constructor(private http: HttpClient) {}
+
+  callBackend() {
+    this.http.get('http://adresse-de-votre-backend/api/hello').subscribe(
+      (response) => {
+        alert(response);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
